338105286675d9ea4b1ad2a41299ba8d6e1bcd42
Aleksander Kolata <olek.tbg.konta@gmail.com>
2022-02-06 23:24:02 +0100

    configured Spring Cloud Config Server to use local git repository

---
+++ config-server/src/main/java/pl/akolata/config/configuration/ConfigurationServerConfiguration.java
@@ -0,0 +1,9 @@
+package pl.akolata.config.configuration;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigServer
+class ConfigurationServerConfiguration {
+}
