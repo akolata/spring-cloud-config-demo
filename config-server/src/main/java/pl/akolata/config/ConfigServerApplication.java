14c13f0d57bf8f4c85842a3b6ffa4d1c61c8e0dd
Aleksander Kolata <olek.tbg.konta@gmail.com>
2022-02-06 22:55:07 +0100

    init project

---
+++ config-server/src/main/java/pl/akolata/config/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package pl.akolata.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
