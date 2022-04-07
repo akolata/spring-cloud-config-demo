2ce267a06249db276955370a522046329e1711a3
Aleksander Kołata <olek.tbg.konta@gmail.com>
2022-04-07 23:46:55 +0200

    unnecessary annotation removed

--- product-service/src/main/java/pl/akolata/config/props/DemoProperties.java
+++ product-service/src/main/java/pl/akolata/config/props/DemoProperties.java
@@ -1,11 +1,9 @@
 package pl.akolata.config.props;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
-@RefreshScope
 @ConfigurationProperties(prefix = "configuration-prop")
 public class DemoProperties {
     private String hello;
